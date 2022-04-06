ccc7fd004c4658b7273d70ed9c1c009eac1a499d
elfinamk24 <karima.elfina@gmail.com>
2022-04-07 05:49:19 +0700

    Project 1 ver 1 commit

---
+++ project1.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Stock Price App Lite Version
+
+Microsoft stock closing price and column!
+
+""")
+
+#define the ticker symbol
+tickerSymbol = 'MSFT'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2012-1-1', end='2021-1-25')
+
+#show data
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
